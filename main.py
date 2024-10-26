9c88d250edc07a96e33b8563a092f83c00f26e9e
Vikriti Lokegaonkar <v@lokegaonkar.in>
2024-10-26 14:05:45 -0700

    Added model db boilerplate

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+from typing import Literal
 import uvicorn
 
 app = FastAPI()
 
+db = None # Init DB Here
+
 class Lesson(BaseModel):
-    pass
+    title: str
+    lesson_text: str
+    solution: str
+    solution_information: str
+    lesson_type: Literal["latex", "linux"]
+    solution_boilerplate: str
+
+class LessonPlan(BaseModel):
+    id: int
+    title: str
+    author: str
+    lessons: list[Lesson]
+
+    @classmethod
+    def get(cls, db, lesson_id: int):
+        # DATABASE CODE HERE
+
+        return cls(
+            id=0,
+            title="Test Lesson",
+            author="John Smith",
+            lessons=[
+                Lesson(title="Lesson 1", lesson_text="Ah", solution="AHH", lesson_type="latex", solution_information="HAHA",solution_boilerplate="Meow")
+            ]
+        )
 
 class SubmissionResult(BaseModel):
     pass
 
-@app.get("/lesson")
-def lessons() -> Lesson:
-    pass
+@app.get("/lesson_plan")
+def lesson(lesson_id: int) -> LessonPlan:
+    return LessonPlan.get(db, lesson_id)
 
 
 @app.post("/check_submission")
