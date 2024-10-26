091cb1ba4bd90f4bd16e4c9448448c794688c8d3
Vikriti Lokegaonkar <v@lokegaonkar.in>
2024-10-26 13:46:00 -0700

    Added template

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from typing import List
+import uvicorn
+
+app = FastAPI()
+
+class Lesson:
+    pass
+
+class SubmissionResult:
+    pass
+
+app.get("/lesson")
+def lessons() -> Lesson:
+    pass
+
+
+app.post("/check_submission")
+def check_submission(lesson_id: int, sublesson_id: int, submission: str) -> SubmissionResult:
+    pass
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
